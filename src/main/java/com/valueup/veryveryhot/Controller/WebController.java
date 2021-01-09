6b6e6e48999003ce562d8b4270939264e0220fc8
baeseongkyu <akak5211@dgu.ac.kr>
2021-01-10 01:52:23 +0900

    requset

--- src/main/java/com/valueup/veryveryhot/Controller/WebController.java
+++ src/main/java/com/valueup/veryveryhot/Controller/WebController.java
@@ -2,12 +2,11 @@
 package com.valueup.veryveryhot.Controller;
 import java.io.IOException;
 
-
 import com.valueup.veryveryhot.S3.S3Uploader;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,7 +19,9 @@ public class WebController {
 
     private final S3Uploader s3Uploader;
 
-    @GetMapping("/")
+    
+
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
