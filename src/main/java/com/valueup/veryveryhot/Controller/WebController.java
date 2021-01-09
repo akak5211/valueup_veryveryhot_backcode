8298f818735da1fe89749766ce8b9a71405d1ea2
baeseongkyu <akak5211@dgu.ac.kr>
2021-01-10 01:34:34 +0900

    체크체크

--- src/main/java/com/valueup/veryveryhot/Controller/WebController.java
+++ src/main/java/com/valueup/veryveryhot/Controller/WebController.java
@@ -1,3 +1,19 @@
+
+package com.valueup.veryveryhot.Controller;
+import java.io.IOException;
+
+
+import com.valueup.veryveryhot.S3.S3Uploader;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Controller
 public class WebController {
--- src/main/java/com/valueup/veryveryhot/S3/S3Uploader.java
+++ src/main/java/com/valueup/veryveryhot/S3/S3Uploader.java
@@ -1,3 +1,20 @@
+package com.valueup.veryveryhot.S3;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
