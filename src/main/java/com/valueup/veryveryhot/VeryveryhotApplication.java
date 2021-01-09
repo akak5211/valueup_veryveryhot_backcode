a5b68c1496c53fcb3a1a535259432c6505e4d638
baeseongkyu <akak5211@dgu.ac.kr>
2021-01-10 02:37:37 +0900

    테스트99

--- src/main/java/com/valueup/veryveryhot/VeryveryhotApplication.java
+++ src/main/java/com/valueup/veryveryhot/VeryveryhotApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VeryveryhotApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+    + "classpath:application.yml,"
+    + "classpath:aws.yml";
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeryveryhotApplication.class, args);
