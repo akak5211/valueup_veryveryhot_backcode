56629f3ffc1cacb246696cdd2b1f5eed0dc4b517
leeyonghee <yhlee2514@daum.net>
2021-01-14 15:59:26 +0900

    컨트롤러 수정 테스트

--- src/main/java/com/valueup/veryveryhot/Controller/UserController.java
+++ src/main/java/com/valueup/veryveryhot/Controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RequestMapping("api/v1/user") //v1 빼니 오류 발생함
+//@RequestMapping(value = "api/v1/user", method = {RequestMethod.GET,RequestMethod.POST}) //v1 빼니 오류 발생함
 @RestController
 
 public class UserController {
@@ -20,20 +20,20 @@ public class UserController {
 
     //Get http methods controllers
     
-    @GetMapping
-    public List<User> getAll(){
+    @RequestMapping(value = "api/v1/user", method = {RequestMethod.GET})
+        public List<User> getAll(){
         return userService.getAllUsers();
     }
 
-    @GetMapping(value = "{id}")
+    @RequestMapping(value = "api/v1/user/{id}", method = {RequestMethod.GET})
     public User getById(@PathVariable("id") String id) {
         return userService.getUser(id);
     }
 
     //Non-Get http method controllers
 
-    @PostMapping
-    public User addUser(@RequestBody User user){
+    @RequestMapping(value = "api/v1/user", method = {RequestMethod.POST})
+        public User addUser(@RequestBody User user){
         return userService.addUser(user);
     }
 
