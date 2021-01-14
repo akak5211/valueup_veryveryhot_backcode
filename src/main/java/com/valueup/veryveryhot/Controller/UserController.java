d040768485a60380457686ad20451bb585e849bc
leeyonghee <yhlee2514@daum.net>
2021-01-14 16:20:12 +0900

    POST API 수정 테스트

--- src/main/java/com/valueup/veryveryhot/Controller/UserController.java
+++ src/main/java/com/valueup/veryveryhot/Controller/UserController.java
@@ -1,10 +1,19 @@
 package com.valueup.veryveryhot.Controller;
 
+import java.util.List;
+
 import com.valueup.veryveryhot.Model.User;
 import com.valueup.veryveryhot.Service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 //@RequestMapping(value = "api/v1/user", method = {RequestMethod.GET,RequestMethod.POST}) //v1 빼니 오류 발생함
 @RestController
@@ -33,11 +42,12 @@ public class UserController {
     //Non-Get http method controllers
 
     @RequestMapping(value = "api/v1/user", method = {RequestMethod.POST})
+    @ResponseStatus(value=HttpStatus.OK)
         public User addUser(@RequestBody User user){
         return userService.addUser(user);
     }
 
-    @DeleteMapping(value = "{id}")
+    @DeleteMapping(value = "api/v1/user/{id}")
     public void removeUser(@PathVariable String id){
         userService.removeUser(id);
     }
