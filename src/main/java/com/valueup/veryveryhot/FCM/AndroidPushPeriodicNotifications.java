bf05191bd3ca69483e68db28a3997eff683dbe1b
leeyonghee <yhlee2514@daum.net>
2021-02-01 22:01:33 +0900

    Revert "getBytes 사용"
    This reverts commit 885907995afda94155971200da4c8db8ecf68cce.

--- src/main/java/com/valueup/veryveryhot/FCM/AndroidPushPeriodicNotifications.java
+++ src/main/java/com/valueup/veryveryhot/FCM/AndroidPushPeriodicNotifications.java
@@ -1,6 +1,5 @@
 package com.valueup.veryveryhot.FCM;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,18 +28,8 @@ public class AndroidPushPeriodicNotifications {
         body.put("registration_ids", array);
 
         JSONObject notification = new JSONObject();
-        String title = "새로운 댓글이 달렸습니다.";
-        byte[] titleByte;
-        byte[] bodyByte;
-        try{
-            titleByte = title.getBytes("UTF-8");
-            bodyByte = qnatitle.getBytes("UTF-8");
-            notification.put("title", titleByte);
-            notification.put("body", bodyByte);
-
-        } catch (UnsupportedEncodingException e){
-            e.printStackTrace();
-        }
+        notification.put("title","새로운 댓글이 달렸습니다.");
+        notification.put("body",qnatitle);
 
         body.put("notification", notification);
 
