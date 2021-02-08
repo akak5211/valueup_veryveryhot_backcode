9c6e6546d359d5ac9836783fc8ed2df9fc52d7ed
baeseongkyu <akak5211@dgu.ac.kr>
2021-02-08 20:33:22 +0900

    page

---
+++ src/main/java/com/valueup/veryveryhot/Controller/PageController.java
@@ -0,0 +1,13 @@
+package com.valueup.veryveryhot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("product")
+    public String index() {
+         return "crossproduct.html";
+    }
+    
+}
