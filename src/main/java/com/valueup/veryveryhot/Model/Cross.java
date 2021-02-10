51904860afb7ad7e7f4f214e1766c86c295ba571
leeyonghee <yhlee2514@daum.net>
2021-02-10 21:03:37 +0900

    Cross/User 수정 / sendscore에 category 추가

--- src/main/java/com/valueup/veryveryhot/Model/Cross.java
+++ src/main/java/com/valueup/veryveryhot/Model/Cross.java
@@ -1,32 +1,11 @@
 package com.valueup.veryveryhot.Model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.data.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-
+@AllArgsConstructor
+@Getter
 public class Cross {
     private final int score;
     private final String category;
-
-
-    @Id
-    private String id;
-
-
-    public Cross(@JsonProperty("score") int score, @JsonProperty("category") String category){
-        this.score = score;
-        this.category = category;
-    }
-
-    public int getScore(){
-        return this.score;
-    }
-
-    public String getCategory(){
-        return this.category;
-    }
-
-    public String getId(){
-        return this.id;
-    }
 }
--- src/main/java/com/valueup/veryveryhot/Model/User.java
+++ src/main/java/com/valueup/veryveryhot/Model/User.java
@@ -1,8 +1,14 @@
 package com.valueup.veryveryhot.Model;
+
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.springframework.data.annotation.Id;
 
+import lombok.Getter;
 
+@Getter
 public class User {
     private final String name;
     private final String userid;
@@ -10,47 +16,22 @@ public class User {
     private final String birthdate;
     private final String emailaddress;
     private final String token;
-    
+    private List<Cross> cross;
+
     @Id
     private String id;
 
-    public User(@JsonProperty("name") String name, @JsonProperty("userid") String userid, 
-    @JsonProperty("password") String password, @JsonProperty("birthdate") String birthdate,
-    @JsonProperty("emailaddress") String emailaddress, @JsonProperty("token") String token){
+    public User(@JsonProperty("name") String name, @JsonProperty("userid") String userid,
+            @JsonProperty("password") String password, @JsonProperty("birthdate") String birthdate,
+            @JsonProperty("emailaddress") String emailaddress, @JsonProperty("token") String token,
+            @JsonProperty("cross") List<Cross> cross) {
         this.name = name;
         this.userid = userid;
         this.password = password;
         this.birthdate = birthdate;
         this.emailaddress = emailaddress;
         this.token = token;
+        this.cross = cross;
     }
 
-    public String getName() {
-        return this.name;
-    }
-
-    public String getUserid() {
-        return this.userid;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public String getBirthdate() {
-        return this.birthdate;
-    }
-
-    public String getEmailaddress() {
-        return this.emailaddress;
-    }
-
-    public String getId() {
-        return this.id;
-    }
-
-    public String getToken() {
-        return this.token;
-    }
-    
 }
