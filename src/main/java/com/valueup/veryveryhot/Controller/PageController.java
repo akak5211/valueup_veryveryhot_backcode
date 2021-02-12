5d49de5a1f7017608ea3658f5c3f7afee397c704
leeyonghee <yhlee2514@daum.net>
2021-02-12 19:25:49 +0900

    정답확인 순서 변경

--- src/main/java/com/valueup/veryveryhot/Controller/PageController.java
+++ src/main/java/com/valueup/veryveryhot/Controller/PageController.java
@@ -10,59 +10,59 @@ public class PageController {
          return "crossproduct.html";
     }
 
-    @RequestMapping("page1")
+    @RequestMapping("page1") //금융상품
     public String page1() {
          return "product";
     }
-    @RequestMapping("page2")
+    @RequestMapping("page2") //보험
     public String page2() {
          return "page2";
     }
-    @RequestMapping("page3")
+    @RequestMapping("page3") //세금
     public String page3() {
          return "세금.htm";
     }
-    @RequestMapping("page4")
+    @RequestMapping("page4") //신용
     public String page4() {
          return "신용.htm";
     }
-    @RequestMapping("page5")
+    @RequestMapping("page5") //연금
     public String page5() {
          return "연금.htm";
     }
-    @RequestMapping("page6")
+    @RequestMapping("page6") //증권
     public String page6() {
          return "증권.htm";
     }
-    @RequestMapping("page7")
+    @RequestMapping("page7") //카드
     public String page7() {
          return "카드.htm";
     }
-    @RequestMapping("page8")
+    @RequestMapping("page8") //펀드
     public String page8() {
          return "펀드.htm";
     }
-    @RequestMapping("page9")
+    @RequestMapping("page9") //일반상식1
     public String page9() {
          return "일반상식1.htm";
     }
-    @RequestMapping("page10")
+    @RequestMapping("page10") //일반상식2
     public String page10() {
          return "일반상식2.htm";
     }
-    @RequestMapping("page11")
+    @RequestMapping("page11") //일반상식3
     public String page11() {
          return "일반상식3.htm";
     }
-    @RequestMapping("page12")
+    @RequestMapping("page12") //세계경제
     public String page12() {
          return "세계경제.htm";
     }
-    @RequestMapping("page13")
+    @RequestMapping("page13") //대출
     public String page13() {
          return "대출.htm";
     }
-    @RequestMapping("page14")
+    @RequestMapping("page14") //저축
     public String page14() {
          return "저축.htm";
     }
